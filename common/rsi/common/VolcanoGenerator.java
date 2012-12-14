1e39900b7ecadf6a17ebb61d00b1bec2524b5abe
StGermain <stgermainmc@gmail.com>
2012-12-13 23:19:27 -0500

    notes

--- common/rsi/common/VolcanoGenerator.java
+++ common/rsi/common/VolcanoGenerator.java
@@ -15,17 +15,17 @@ public class VolcanoGenerator implements IWorldGenerator {
 	public void generate(Random random, int chunkX, int chunkZ, World world,
 				IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
 		BiomeGenBase b = world.getBiomeGenForCoords(chunkX*16, chunkZ*16);
-		int randDefault = -1;
+		int randDefault = -1; //Default chance 1/-1 = No chance
 		int yHeightBase = 0;
 		if(b.biomeName.equals("Ocean")) {
-			yHeightBase = 40; 
-			randDefault = 20;
+			yHeightBase = 40;  
+			randDefault = 20; //Ocean chance 1/20
 		} else if(b.biomeName.equals("Ice Mountains")||b.biomeName.equals("Extreme Hills")) {
 			yHeightBase = 130;
-			randDefault = 30;
+			randDefault = 30; //Chance 1/30
 		}
 		Random randGenny = new Random();
-		int randChance = randGenny.nextInt(randDefault);
+		int randChance = randGenny.nextInt(randDefault); //check if it generates
 		if(randChance < 1 && randChance > 0) {
 			int yHeight = yHeightBase - randGenny.nextInt(20);
 			int yBase = 10 + randGenny.nextInt(20);
