320f79201c7f717126cb85c5868a8342e5ba4a1c
StGermain <stgermainmc@gmail.com>
2012-12-17 22:44:31 -0500

    Updates to EVERYTHING

---
+++ common/redlogic/common/gates/gateDefault.java
@@ -0,0 +1,14 @@
+package rsi.common.gates;
+
+import net.minecraft.src.BlockDirectional;
+import net.minecraft.src.Material;
+
+public class gateDefault extends BlockDirectional {
+
+	protected gateDefault(int par1, boolean par2) {
+		super(par1, 6, Material.circuits);
+		// TODO Auto-generated constructor stub
+	}
+
+
+}
---
+++ common/redlogic/common/items/itemDefault.java
@@ -0,0 +1,18 @@
+package rsi.common.items;
+
+import net.minecraft.src.CreativeTabs;
+import net.minecraft.src.Item;
+
+public class itemDefault extends Item {
+
+	public itemDefault(int par1) {
+		super(par1);
+		maxStackSize = 16;
+        this.setCreativeTab(CreativeTabs.tabRedstone);
+	}
+	
+	public String getTextureFile(){
+		return "/rsi/gfx/items/items.png";
+	}
+
+}
